035382dfcdbd9c3ebf3144f77f474acc70356406
DanielQuispeT <74209762+DanielQuispeT@users.noreply.github.com>
2020-11-10 10:29:52 -0400

    Add files via upload

---
+++ sprincipal01/pprincipal01/principal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Nombre: Quispe Taboada Daniel" << endl;
+	cout << "Carrera: Ingenieria de Sistemas" << endl;
+	cout << "Fecha de nacimiento: 17/11/2000" << endl;
+	cout << "Carnet Universitario: 35-5028" << endl;
+	system("pause");
+	return 0;
+}
